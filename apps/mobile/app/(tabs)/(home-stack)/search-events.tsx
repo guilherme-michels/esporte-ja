227f5fa478b73d7b36cbda31da114ca5620b2617
guimichels <guilherme.michels@universo.univates.br>
2024-10-09 18:31:58 -0300

    changes

--- apps/mobile/app/(tabs)/(home-stack)/search-events.tsx
+++ apps/mobile/app/(tabs)/(home-stack)/search-events.tsx
@@ -1,47 +1,130 @@
 import { trpc } from "@/api";
-import React from "react";
-import { SafeAreaView, Text, FlatList, View } from "react-native";
+import { useDebounce } from "@/hooks/useDebounce";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import React, { useState } from "react";
+import {
+	FlatList,
+	Image,
+	SafeAreaView,
+	ScrollView,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from "react-native";
+import type { Event } from "../../../../api/schemas";
 
 export default function SearchEventsScreen() {
+	const [searchQuery, setSearchQuery] = useState("");
+	const debouncedQuery = useDebounce(searchQuery, 0);
+	const router = useRouter();
+
 	const {
-		data: events,
-		isLoading,
-		error,
-	} = trpc.event.getAll.useQuery({ cursor: "", limit: 10 });
+		data: searchData,
+		isLoading: isSearchLoading,
+		error: searchError,
+		fetchNextPage: fetchNextSearchPage,
+		hasNextPage: hasNextSearchPage,
+	} = trpc.event.search.useInfiniteQuery(
+		{ query: debouncedQuery, limit: 10 },
+		{
+			getNextPageParam: (lastPage) => lastPage.nextCursor,
+			enabled: debouncedQuery.length > 0,
+		},
+	);
 
-	if (isLoading) {
-		return (
-			<SafeAreaView>
-				<Text className="text-white">Carregando...</Text>
-			</SafeAreaView>
-		);
-	}
+	const {
+		data: allEventsData,
+		isLoading: isAllEventsLoading,
+		error: allEventsError,
+		fetchNextPage: fetchNextAllEventsPage,
+		hasNextPage: hasNextAllEventsPage,
+	} = trpc.event.getAll.useInfiniteQuery(
+		{ limit: 10 },
+		{
+			getNextPageParam: (lastPage) => lastPage.nextCursor,
+			enabled: debouncedQuery.length === 0,
+		},
+	);
 
-	if (error) {
-		return (
-			<SafeAreaView>
-				<Text className="text-white">
-					Erro ao carregar empresas: {error.message}
+	const events =
+		debouncedQuery.length > 0
+			? (searchData?.pages.flatMap((page) => page.events) ?? [])
+			: (allEventsData?.pages.flatMap((page) => page.events) ?? []);
+
+	const isLoading = isSearchLoading || isAllEventsLoading;
+	const error = searchError || allEventsError;
+
+	const renderEventItem = ({ item }: { item: Event }) => (
+		<TouchableOpacity
+			style={{
+				flex: 1,
+				margin: 8,
+				display: "flex",
+				flexDirection: "row",
+				justifyContent: "space-between",
+				alignItems: "flex-start",
+				backgroundColor: "#3b82f6",
+				padding: 12,
+				borderRadius: 12,
+			}}
+			onPress={() => router.push(`/(tabs)/(home-stack)/event?id=${item.id}`)}
+		>
+			<Image
+				source={{
+					uri: "https://itaguara.com/wp-content/uploads/2023/06/inscricoes-abertas.jpg",
+				}}
+				className="w-16 h-16 rounded-full mr-4"
+			/>
+
+			<View className="flex-1">
+				<Text className="text-white text-lg font-semibold">{item.title}</Text>
+				<Text className="text-gray-200">
+					{new Date(item.dateTime).toLocaleDateString()}
 				</Text>
-			</SafeAreaView>
-		);
-	}
+			</View>
+			<Ionicons name="chevron-forward" size={24} color="gray" />
+		</TouchableOpacity>
+	);
+
+	const handleLoadMore = () => {
+		if (debouncedQuery.length > 0) {
+			hasNextSearchPage && fetchNextSearchPage();
+		} else {
+			hasNextAllEventsPage && fetchNextAllEventsPage();
+		}
+	};
 
 	return (
 		<SafeAreaView className="flex-1">
-			<Text className="text-white text-xl font-bold p-4">
-				Lista de Empresas
-			</Text>
-			<FlatList
-				data={events?.events}
-				keyExtractor={(item) => item.id}
-				renderItem={({ item }) => (
-					<View className="p-4 border-b border-gray-300">
-						<Text className="text-white text-lg">{item.name}</Text>
-						<Text className="text-gray-400">{item.name}</Text>
-					</View>
-				)}
-			/>
+			<ScrollView className="bg-white h-full">
+				<View className="p-4">
+					<TextInput
+						className="p-3 rounded-lg mb-4 border-2 border-zinc-300"
+						placeholder="Buscar eventos..."
+						placeholderTextColor={"#777777"}
+						value={searchQuery}
+						onChangeText={setSearchQuery}
+					/>
+					{isLoading && <Text className="text-white">Carregando...</Text>}
+					{error && <Text className="text-red-500">Erro: {error.message}</Text>}
+					<FlatList
+						data={events}
+						renderItem={renderEventItem}
+						keyExtractor={(item) => item.id}
+						onEndReached={handleLoadMore}
+						onEndReachedThreshold={0.5}
+						ListEmptyComponent={() =>
+							!isLoading && (
+								<Text className="text-white text-center">
+									Nenhum evento encontrado.
+								</Text>
+							)
+						}
+					/>
+				</View>
+			</ScrollView>
 		</SafeAreaView>
 	);
 }
